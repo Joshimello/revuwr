d3799f31a697325e910db9041b79c68298832884
Joshimello <joshualeanjw@gmail.com>
2024-09-14 10:39:51 +0800

    test: removing admin route guard

--- apps/admin/src/hooks.server.ts
+++ apps/admin/src/hooks.server.ts
@@ -1,33 +1,33 @@
-import { PUBLIC_BASE_PATH, PUBLIC_PB_URL } from "$env/static/public"
-import Pocketbase from "pocketbase"
-import { type TypedPocketBase } from "$lib/pocketbase/pocketbase-types"
-import { error } from "@sveltejs/kit"
+// import { PUBLIC_BASE_PATH, PUBLIC_PB_URL } from "$env/static/public"
+// import Pocketbase from "pocketbase"
+// import { type TypedPocketBase } from "$lib/pocketbase/pocketbase-types"
+// import { error } from "@sveltejs/kit"
 
-export const handle = async ({ event, resolve }) => {
-  const pb = new Pocketbase(PUBLIC_PB_URL) as TypedPocketBase
-  pb.authStore.loadFromCookie(event.request.headers.get('cookie') || '')
+// export const handle = async ({ event, resolve }) => {
+//   const pb = new Pocketbase(PUBLIC_PB_URL) as TypedPocketBase
+//   pb.authStore.loadFromCookie(event.request.headers.get('cookie') || '')
 
-  if (event.url.pathname.startsWith(`/auth`)) {
-    return await resolve(event)
-  }
+//   if (event.url.pathname.startsWith(`/auth`)) {
+//     return await resolve(event)
+//   }
 
-  if (pb.authStore.isValid && pb.authStore.isAdmin) {
-    const response = await resolve(event)
-    try {
-      await pb.admins.authRefresh()
-      response.headers.append(
-        'set-cookie', 
-        pb.authStore.exportToCookie({ 
-          secure: false, 
-          httpOnly: false 
-        })
-      )
-    }
-    catch (err) {
-      return error(401, 'Unauthorized')
-    }
-    return response
-  }
+//   if (pb.authStore.isValid && pb.authStore.isAdmin) {
+//     const response = await resolve(event)
+//     try {
+//       await pb.admins.authRefresh()
+//       response.headers.append(
+//         'set-cookie', 
+//         pb.authStore.exportToCookie({ 
+//           secure: false, 
+//           httpOnly: false 
+//         })
+//       )
+//     }
+//     catch (err) {
+//       return error(401, 'Unauthorized')
+//     }
+//     return response
+//   }
 
-  return error(401, 'Unauthorized')
-}
+//   return error(401, 'Unauthorized')
+// }
