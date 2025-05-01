d7c426479aa0929e48b7eada6d57d3bbaa939cae
joshimello <joshualeanjw@gmail.com>
2025-05-01 23:20:33 +0800

    feat: client translation for questions

--- apps/platform/src/routes/(main)/application/[id]/question.svelte
+++ apps/platform/src/routes/(main)/application/[id]/question.svelte
@@ -5,6 +5,7 @@
 	import * as Tooltip from '$lib/components/ui/tooltip';
 	import { Info } from 'lucide-svelte';
 	import { toast } from 'svelte-sonner';
+	import { m } from '$lib/paraglide/messages.js';
 	import { updateAnswer } from './methods';
 	import questionTypes from './question-types';
 	import { answers, application, currentIndex, isReadOnly } from './stores';
@@ -23,9 +24,9 @@
 	{#key value}
 		<div class="flex gap-2">
 			{#if question.required}
-				<Badge variant="outline">Required</Badge>
+			<Badge variant="outline">{m.required_badge()}</Badge>
 			{:else}
-				<Badge variant="secondary">Optional</Badge>
+			<Badge variant="secondary">{m.optional_badge()}</Badge>
 			{/if}
 
 			{#if checkValid && !checkValid()[0]}
@@ -55,7 +56,7 @@
 
 	{#if $answers[$currentIndex].comment}
 		<Alert.Root variant="destructive">
-			<Alert.Title>Comments</Alert.Title>
+		<Alert.Title>{m.alert_comments_title()}</Alert.Title>
 			<Alert.Description>
 				{$answers[$currentIndex].comment}
 			</Alert.Description>
@@ -66,20 +67,20 @@
 		{#key value}
 			<div class="sticky bottom-20 mt-24 flex items-center gap-2 md:bottom-24">
 				{#if $currentIndex < $answers.length - 1}
-					<Button
-						size="lg"
-						on:click={async () => {
-							const [status, res] = await updateAnswer(content.id, value);
-							if (status) {
-								$currentIndex = $currentIndex + 1;
-							} else {
-								toast.error('Failed to update answer');
-							}
-						}}
-						disabled={checkValid && !checkValid()[0]}
-					>
-						Continue
-					</Button>
+				<Button
+										size="lg"
+										on:click={async () => {
+											const [status, res] = await updateAnswer(content.id, value);
+											if (status) {
+												$currentIndex = $currentIndex + 1;
+											} else {
+												toast.error(m.error_update_answer());
+											}
+										}}
+										disabled={checkValid && !checkValid()[0]}
+									>
+										{m.button_continue()}
+									</Button>
 				{:else}
 					<form
 						method="post"
@@ -89,7 +90,7 @@
 							const [status, res] = await updateAnswer(content.id, value);
 							if (status) {
 								isLoading = true;
-								toast.loading('Submitting application...', {
+								toast.loading(m.toast_submitting_application(), {
 									duration: Number.POSITIVE_INFINITY
 								});
 							} else {
@@ -98,19 +99,19 @@
 							}
 						}}
 					>
-						<Button
-							size="lg"
-							disabled={isLoading ||
-								(checkValid &&
-									!checkValid()[0] &&
-									!(
-										$currentIndex === $answers.length - 1 &&
-										$application?.status === 'editsRequested'
-									))}
-							type="submit"
-						>
-							Submit Application
-						</Button>
+					<Button
+												size="lg"
+												disabled={isLoading ||
+													(checkValid &&
+														!checkValid()[0] &&
+														!(
+															$currentIndex === $answers.length - 1 &&
+															$application?.status === 'editsRequested'
+														))}
+												type="submit"
+											>
+												{m.button_submit_application()}
+											</Button>
 					</form>
 				{/if}
 
@@ -120,7 +121,7 @@
 							<Info size="14" />
 						</Button>
 					</Tooltip.Trigger>
-					<Tooltip.Content>Question will be saved once you click continue</Tooltip.Content>
+					<Tooltip.Content>{m.tooltip_question_saved()}</Tooltip.Content>
 				</Tooltip.Root>
 			</div>
 		{/key}
@@ -128,5 +129,5 @@
 
 	<div class="h-96"></div>
 {:else}
-	Invalid question
+{m.invalid_question()}
 {/if}
