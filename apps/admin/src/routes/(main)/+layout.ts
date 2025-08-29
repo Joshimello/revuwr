f54042cf83b0ab0bdeeaa12062495309fd02d1e9
joshimello <joshualeanjw@gmail.com>
2025-08-29 10:53:01 +0800

    feat: fix custom acc login

--- apps/admin/src/routes/(main)/+layout.ts
+++ apps/admin/src/routes/(main)/+layout.ts
@@ -2,8 +2,4 @@ import { pb } from '$lib/pocketbase/client';
 import { error } from '@sveltejs/kit';
 import type { PageLoad } from './$types';
 
-export const load: PageLoad = async () => {
-	if (!(pb.authStore.isValid && pb.authStore.isSuperuser)) {
-		return error(401, 'Unauthorized');
-	}
-};
+export const load: PageLoad = async () => {};
